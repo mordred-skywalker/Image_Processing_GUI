3237477cfb402976935ce9af04aff8e148b238e0
mordred-skywalker <86327611+mordred-skywalker@users.noreply.github.com>
2021-12-16 23:11:17 -0500

    Add unit testing file

---
+++ test_GUI.py
@@ -0,0 +1,24 @@
+from Image_Processor import *
+from main_GUI import *
+
+# We used this code for testing. However, since our code
+# has a lot of output of images. We manually checked that
+# the generated image are correct (correctly captures the
+# centers).
+
+
+# if __name__ == '__main__':
+
+#     img, coord = img_process('/Users/mordredyuan/Downloads/htcpID617_well02_P-21-11-10_stitch1_c4_1x_.tiff')
+
+#     show_cropped_image(img, coord[1][0], coord[1][1], 100)
+
+#     show_cropped_image(img, coord[1][0], coord[1][1], 100)
+
+#     show_cropped_image(img, coord[1][0], coord[1][1], 100)
+
+#     plt.imshow(img)
+
+#     print(coord)
+
+#     main()
